b32b9e3431bb95df4b7b6f6231bd272f7c0d1088
Davi <davi.r.vilarinho@gmail.com>
2024-04-04 10:39:53 -0300

    reorganizing in files

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,14 @@
 <template>
-  <div>
-    <h1 class="title">QUIZ TIME!</h1>
-  </div>
-  <quiz-question v-for="question in questions" :key="question['q']">
-    <template v-slot:question>
-      <h2>{{ question['q'] }}</h2>
-    </template>
-
-    <template v-slot:answers>
-      <quiz-answer v-for="answer in question['answers']" :key="answer['text']" 
-        :text="answer['text']"
-        :isCorrect="answer['is_correct']"
-        :questionId="question['q']"
-        :selectedAnswerInQuestion="correctAnswered[question['q']]?.selectedAnswer"
-        :isSubmitted="isSubmitted"
-        @question-answered="markAnswered">
-      </quiz-answer>
-    </template>
-  </quiz-question>
-  <div>
-    <button class="btn reset-btn" @click.prevent="reset">Reset</button>
-    <button class="btn submit-btn" @click.prevent="submit">Submit</button>
+  <div class="ctr">
+    <button type="button" class="reset-btn">Reset</button>
   </div>
 </template>
 
 <script>
-import QuizAnswer from './components/QuizAnswer.vue';
-import QuizQuestion from './components/QuizQuestion.vue';
-
 export default {
   name: 'App',
   data() {
     return {
-      isSubmitted: false,
-      correctAnswered: {},
       questions: [
         {
           q: 'What is 2 + 2?',
@@ -112,121 +87,8 @@ export default {
     }
   },
   components: {
-    QuizQuestion,
-    QuizAnswer
   },
   methods: {
-    markAnswered(answerObject) {
-      this.correctAnswered[answerObject['questionId']] = answerObject;
-    },
-    reset() {
-      this.correctAnswered = {};
-      this.isSubmitted = false;
-    },
-    submit() {
-      this.isSubmitted = true;
-    }
   },
 }
 </script>
-<style>
-* {
-  box-sizing: border-box;
-}
-
-body {
-  font-size: 20px;
-  font-family: sans-serif;
-  padding-top: 20px;
-  background: #e6ecf1;
-}
-
-.progress {
-  height: 50px;
-  margin-top: 10px;
-  background-color: #ddd;
-  position: relative;
-}
-
-.bar {
-  height: 50px;
-  background-color: #ff6372;
-  transition: all 0.3s linear;
-}
-
-.status {
-  position: absolute;
-  top: 15px;
-  left: 0;
-  text-align: center;
-  color: #fff;
-  width: 100%;
-}
-
-.title {
-  width: 100%;
-  padding: 20px;
-  font-size: 32px;
-  font-weight: bold;
-  text-align: center;
-  background-color: #12cbc4;
-  color: #fff;
-  box-sizing: border-box;
-}
-
-.desc {
-  border: 1px solid #8e959f;
-  padding: 20px;
-  font-size: 18px;
-  width: 100%;
-  background-color: #fff;
-  transition: 0.4s linear all;
-  text-align: center;
-}
-
-.fade-enter-from {
-  opacity: 0;
-}
-
-.fade-enter-active {
-  transition: all 0.3s linear;
-}
-
-.fade-leave-active {
-  transition: all 0.3s linear;
-  opacity: 0;
-  position: absolute;
-}
-
-.result {
-  width: 100%;
-}
-
-.btn {
-  border: 1px;
-  font-size: 22px;
-  color: #fff;
-  padding: 10px 25px;
-  margin: 10px auto;
-  display: block;
-}
-
-.reset-btn {
-  background-color: #ff6372;
-  border-color: #ef6272;
-}
-
-.submit-btn {
-  background-color: #8ce200;
-  border-color: #7ce000;
-}
-
-
-.reset-btn:active,
-.reset-btn:focus,
-.reset-btn:hover {
-  border: 0;
-  outline: 0;
-}
-
-</style>
