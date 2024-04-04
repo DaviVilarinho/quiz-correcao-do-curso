1b48fcf5fea2332683f681307bc0dd89308b4463
Davi <davi.r.vilarinho@gmail.com>
2024-04-04 11:29:38 -0300

    fix typo is_correct

--- src/components/QuestionsVue.vue
+++ src/components/QuestionsVue.vue
@@ -1,26 +1,21 @@
 <template>
-    <div class="questions-ctr">
-      <div class="progress">
-        <div class="bar"
-          :style="{ width: `${questionsAnswered/questions.length * 100}%` }"
-        >
-        </div>
-        <div class="status">{{ questionsAnswered }} out of {{ questions.length }} questions answered</div>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div class="bar" :style="{ width: `${questionsAnswered / questions.length * 100}%` }">
       </div>
-      <div class="single-question" 
-        v-for="(question, index) in questions" 
-        :key="question.q" 
+      <div class="status">{{ questionsAnswered }} out of {{ questions.length }} questions answered</div>
+    </div>
+    <TransitionGroup name="fade">
+      <div class="single-question" v-for="(question, index) in questions" :key="question.q"
         v-show="index === questionsAnswered">
         <div class="question">{{ question.q }}</div>
         <div class="answers">
-          <div class="answer" 
-            v-for="answer in question.answers"
-            :key="answer.text"
-            @click.prevent="selectAnswer(answer.isCorrect)"
-            >{{ answer.text }}</div>
+          <div class="answer" v-for="answer in question.answers" :key="answer.text"
+            @click.prevent="selectAnswer(answer.is_correct)">{{ answer.text }}</div>
         </div>
       </div>
-    </div>
+    </TransitionGroup>
+  </div>
 </template>
 
 <script>
